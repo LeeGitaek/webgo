a6975fc18228b5d694f81206aaabddcc8d8020ed
GitaekLee <coolfamily77@gmail.com>
2020-02-10 10:26:32 -0800

    Add - Get parameter function - Get query function
    Set up
    - Echo web framework for golang.

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-"net/http"
-"github.com/labstack/echo"
+	"github.com/labstack/echo"
+	"net/http"
 )
 
 func main() {
@@ -11,7 +11,19 @@ func main() {
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello World!")
 	})
+	e.GET("/users/:id", getUser)
+	e.GET("/show", show)
 
 	e.Logger.Fatal(e.Start(":1323")) // localhost:1323
 
+}
+
+func getUser(c echo.Context) error {
+	id := c.Param("id")
+	return c.String(http.StatusOK, id)
+}
+func show(c echo.Context) error {
+	team := c.QueryParam("team")
+	member := c.QueryParam("member")
+	return c.String(http.StatusOK, "team :"+team+",member :"+member)
 }
