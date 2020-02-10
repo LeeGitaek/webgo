f5a31ab255cc4cd16b07250e04215a494d1646a7
GitaekLee <coolfamily77@gmail.com>
2020-02-10 10:42:59 -0800

    Add - POST method function ( /save , name + email )

--- main.go
+++ main.go
@@ -13,17 +13,33 @@ func main() {
 	})
 	e.GET("/users/:id", getUser)
 	e.GET("/show", show)
+	e.POST("/save", save)
 
 	e.Logger.Fatal(e.Start(":1323")) // localhost:1323
 
 }
 
 func getUser(c echo.Context) error {
+	// localhost:1323/users/gitaeklee
+	// output ( method : GET )
+	// gitaeklee
 	id := c.Param("id")
 	return c.String(http.StatusOK, id)
 }
 func show(c echo.Context) error {
+	// localhost:1323/show?team=divtag&member=gitaeklee
+	// output ( method : GET )
+	// team : divtag, member : gitaeklee
 	team := c.QueryParam("team")
 	member := c.QueryParam("member")
 	return c.String(http.StatusOK, "team :"+team+",member :"+member)
+}
+
+func save(c echo.Context) error {
+	// localhost:1323/save
+	// output ( method : POST )
+	// name :Joe Smith, email :joe@labstack.com
+	name := c.FormValue("name")
+	email := c.FormValue("email")
+	return c.String(http.StatusOK, "name :"+name+", email :"+email+"\n")
 }
