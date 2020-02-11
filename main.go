a01160e927798c1490e88fa58653564dfec50741
GitaekLee <coolfamily77@gmail.com>
2020-02-11 00:59:54 -0800

    Add - handlers / GetTask,PutTask,DeleteTask function - models / GetTask,PutTask,DeleteTask function

--- main.go
+++ main.go
@@ -1,16 +1,34 @@
 package main
 
 import (
+	"database/sql"
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	_ "github.com/mattn/go-sqlite3"
 	"net/http"
+	"webgo/handlers"
+	//"github.com/graphql-go/graphql"
+	//"encoding/json"
 )
 
 func main() {
+	//schema, _ := graphql.NewSchema(graphql.SchemaConfig{})
 
+	db := initDB("storage.db")
+	migrate(db)
+
+	// new instance.
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello World!")
-	})
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.File("/", "public/index.html")
+	e.GET("/tasks", handlers.GetTasks(db))
+	e.PUT("/tasks", handlers.PutTask(db))
+	e.DELETE("/tasks/:id", handlers.DeleteTask(db))
+
 	e.GET("/users/:id", getUser)
 	e.GET("/show", show)
 	e.POST("/save", save)
@@ -42,4 +60,30 @@ func save(c echo.Context) error {
 	name := c.FormValue("name")
 	email := c.FormValue("email")
 	return c.String(http.StatusOK, "name :"+name+", email :"+email+"\n")
+}
+
+func initDB(filepath string) *sql.DB {
+	db, err := sql.Open("sqlite3", filepath)
+	if err != nil { // if err is not nil
+		panic(err)
+	}
+
+	if db == nil { // if db is nil
+		panic("db nil")
+	}
+	return db
+}
+
+func migrate(db *sql.DB) {
+	sql := `CREATE TABLE IF NOT EXISTS tasks(
+        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+        name VARCHAR NOT NULL );`
+
+	_, err := db.Exec(sql)
+
+	// Exit if something goes wrong with our SQL statement above
+	if err != nil {
+		panic(err)
+	}
+
 }
